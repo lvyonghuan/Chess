f44397b961155d07c871916deb06dee17b5b235e
lvyonghuan <110887355+lvyonghuan@users.noreply.github.com>
2023-06-11 02:28:59 +0800

    Update chess.go

--- service/chess.go
+++ service/chess.go
@@ -27,11 +27,11 @@ func checkMove(c *model.Client, msg model.WebsocketMessage) (bool, string) {
 	case model.Queen:
 		return checkQueenMove(x1, y1, x2, y2, color, checkerBoard)
 	case model.Rook:
-
+		return checkRookMove(x1, y1, x2, y2, color, checkerBoard)
 	case model.Bishop:
-
+		return checkBishopMove(x1, y1, x2, y2, color, checkerBoard)
 	case model.Knight:
-
+		return checkKnightMove(x1, y1, x2, y2, color, checkerBoard)
 	case model.Pawn:
 
 	default:
@@ -108,9 +108,8 @@ func checkQueenMove(x1, y1, x2, y2, color int, checkerBoard *model.Chess) (bool,
 		if checkerBoard.Checkerboard[x2][y2][0] != 0 && checkerBoard.Checkerboard[x2][y2][1] == color {
 			return false, "目标位置存在己方棋子"
 		}
-
+		// 执行临时移动
 		tempBoard := copyChessBoard(checkerBoard)
-		// 执行临时移动。
 		tempBoard.Checkerboard[x1][y1][0], tempBoard.Checkerboard[x2][y2][0] = model.Void, tempBoard.Checkerboard[x1][y1][0]
 		tempBoard.Checkerboard[x1][y1][1], tempBoard.Checkerboard[x2][y2][1] = model.Void, tempBoard.Checkerboard[x1][y1][1]
 		CalculateThreaten(color, &tempBoard)
@@ -119,4 +118,166 @@ func checkQueenMove(x1, y1, x2, y2, color int, checkerBoard *model.Chess) (bool,
 		}
 	}
 	return true, ""
+}
+
+func checkRookMove(x1, y1, x2, y2, color int, checkerBoard *model.Chess) (bool, string) {
+	var flag = returnFlag(color)
+	dx := x2 - x1
+	dy := y2 - y1
+
+	if dx != 0 && dy != 0 {
+		return false, "车只能沿水平或垂直线移动"
+	}
+
+	stepX := 0
+	if dx != 0 {
+		stepX = dx / abs(dx) // 确定 x 方向
+	}
+
+	stepY := 0
+	if dy != 0 {
+		stepY = dy / abs(dy) // 确定 y 方向
+	}
+
+	x := x1 + stepX
+	y := y1 + stepY
+
+	for x != x2 || y != y2 {
+		if checkerBoard.Checkerboard[x][y][0] != 0 {
+			return false, "移动路径被阻挡"
+		}
+		x += stepX
+		y += stepY
+	}
+
+	if checkerBoard.Checkerboard[x2][y2][0] != 0 && checkerBoard.Checkerboard[x2][y2][1] == color {
+		return false, "目标位置存在己方棋子"
+	}
+	// 执行临时移动
+	tempBoard := copyChessBoard(checkerBoard)
+	tempBoard.Checkerboard[x1][y1][0], tempBoard.Checkerboard[x2][y2][0] = model.Void, tempBoard.Checkerboard[x1][y1][0]
+	tempBoard.Checkerboard[x1][y1][1], tempBoard.Checkerboard[x2][y2][1] = model.Void, tempBoard.Checkerboard[x1][y1][1]
+	CalculateThreaten(color, &tempBoard)
+
+	if tempBoard.Checkerboard[tempBoard.King[color][0]][tempBoard.King[color][1]][flag] == 1 {
+		return false, "王有威胁"
+	}
+
+	return true, ""
+}
+
+func checkBishopMove(x1, y1, x2, y2, color int, checkerBoard *model.Chess) (bool, string) {
+	var flag = returnFlag(color)
+	dx := x2 - x1
+	dy := y2 - y1
+
+	if abs(dx) != abs(dy) {
+		return false, "象只能沿对角线移动"
+	}
+	stepX := dx / abs(dx) // 确定 x 方向
+	stepY := dy / abs(dy) // 确定 y 方向
+
+	x := x1 + stepX
+	y := y1 + stepY
+
+	for x != x2 || y != y2 {
+		if checkerBoard.Checkerboard[x][y][0] != 0 {
+			return false, "移动路径被阻挡"
+		}
+		x += stepX
+		y += stepY
+	}
+
+	if checkerBoard.Checkerboard[x2][y2][0] != 0 && checkerBoard.Checkerboard[x2][y2][1] == color {
+		return false, "目标位置存在己方棋子"
+	}
+	// 执行临时移动
+	tempBoard := copyChessBoard(checkerBoard)
+	tempBoard.Checkerboard[x1][y1][0], tempBoard.Checkerboard[x2][y2][0] = model.Void, tempBoard.Checkerboard[x1][y1][0]
+	tempBoard.Checkerboard[x1][y1][1], tempBoard.Checkerboard[x2][y2][1] = model.Void, tempBoard.Checkerboard[x1][y1][1]
+	CalculateThreaten(color, &tempBoard)
+
+	if tempBoard.Checkerboard[tempBoard.King[color][0]][tempBoard.King[color][1]][flag] == 1 {
+		return false, "王有威胁"
+	}
+	return true, ""
+}
+
+func checkKnightMove(x1, y1, x2, y2, color int, checkerBoard *model.Chess) (bool, string) {
+	var flag = returnFlag(color)
+	dx := abs(x2 - x1)
+	dy := abs(y2 - y1)
+
+	if (dx != 2 || dy != 1) && (dx != 1 || dy != 2) {
+		return false, "马的移动方式为 'L' 形"
+	}
+
+	if checkerBoard.Checkerboard[x2][y2][0] != 0 && checkerBoard.Checkerboard[x2][y2][1] == color {
+		return false, "目标位置存在己方棋子"
+	}
+	// 执行临时移动
+	tempBoard := copyChessBoard(checkerBoard)
+	tempBoard.Checkerboard[x1][y1][0], tempBoard.Checkerboard[x2][y2][0] = model.Void, tempBoard.Checkerboard[x1][y1][0]
+	tempBoard.Checkerboard[x1][y1][1], tempBoard.Checkerboard[x2][y2][1] = model.Void, tempBoard.Checkerboard[x1][y1][1]
+	CalculateThreaten(color, &tempBoard)
+	if tempBoard.Checkerboard[tempBoard.King[color][0]][tempBoard.King[color][1]][flag] == 1 {
+		return false, "王有威胁"
+	}
+
+	return true, ""
+}
+
+func checkPawnMove(x1, y1, x2, y2, color int, checkerBoard *model.Chess) (bool, string) {
+	var flag = returnFlag(color)
+	dx := x2 - x1
+	dy := y2 - y1
+	isFirstMove := (color == model.White && y1 == 1) || (color == model.Black && y1 == 6)
+
+	// Check for one square move
+	if dx == 0 && ((dy == -1 && color == model.Black) || (dy == 1 && color == model.White)) {
+		if checkerBoard.Checkerboard[x2][y2][0] == 0 {
+			tempBoard := copyChessBoard(checkerBoard)
+			tempBoard.Checkerboard[x1][y1][0], tempBoard.Checkerboard[x2][y2][0] = model.Void, tempBoard.Checkerboard[x1][y1][0]
+			tempBoard.Checkerboard[x1][y1][1], tempBoard.Checkerboard[x2][y2][1] = model.Void, tempBoard.Checkerboard[x1][y1][1]
+			CalculateThreaten(color, &tempBoard)
+			if tempBoard.Checkerboard[tempBoard.King[color][0]][tempBoard.King[color][1]][flag] == 1 {
+				return false, "王有威胁"
+			}
+			return true, ""
+		}
+		return false, "不能将己方棋子移动到目标位置"
+	}
+
+	// Check for two square move on first move
+	if isFirstMove && dx == 0 && ((dy == -2 && color == model.Black) || (dy == 2 && color == model.White)) {
+		if checkerBoard.Checkerboard[x2][y2][0] == 0 && checkerBoard.Checkerboard[x2][y2-1][0] == 0 {
+			tempBoard := copyChessBoard(checkerBoard)
+			tempBoard.Checkerboard[x1][y1][0], tempBoard.Checkerboard[x2][y2][0] = model.Void, tempBoard.Checkerboard[x1][y1][0]
+			tempBoard.Checkerboard[x1][y1][1], tempBoard.Checkerboard[x2][y2][1] = model.Void, tempBoard.Checkerboard[x1][y1][1]
+			CalculateThreaten(color, &tempBoard)
+			if tempBoard.Checkerboard[tempBoard.King[color][0]][tempBoard.King[color][1]][flag] == 1 {
+				return false, "王有威胁"
+			}
+			return true, ""
+		}
+		return false, "不能将己方棋子移动到目标位置"
+	}
+
+	// Check for diagonal captures
+	if abs(dx) == 1 && ((dy == -1 && color == model.Black) || (dy == 1 && color == model.White)) {
+		if checkerBoard.Checkerboard[x2][y2][0] != 0 && checkerBoard.Checkerboard[x2][y2][1] != color {
+			tempBoard := copyChessBoard(checkerBoard)
+			tempBoard.Checkerboard[x1][y1][0], tempBoard.Checkerboard[x2][y2][0] = model.Void, tempBoard.Checkerboard[x1][y1][0]
+			tempBoard.Checkerboard[x1][y1][1], tempBoard.Checkerboard[x2][y2][1] = model.Void, tempBoard.Checkerboard[x1][y1][1]
+			CalculateThreaten(color, &tempBoard)
+			if tempBoard.Checkerboard[tempBoard.King[color][0]][tempBoard.King[color][1]][flag] == 1 {
+				return false, "王有威胁"
+			}
+			return true, ""
+		}
+		return false, "兵需要对角线上有一个棋子才能斜线移动"
+	}
+
+	return false, "兵的移动方式不符合规则"
+
 }
